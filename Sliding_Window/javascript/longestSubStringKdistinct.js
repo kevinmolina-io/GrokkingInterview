77ce707d5500060ede7da37315eb483bd1e4f10e
Kevin Molina <kevmo1987@gmail.com>
2023-09-05 18:01:12 -0700

    added tests

--- Sliding_Window/javascript/longestSubStringKdistinct.js
+++ Sliding_Window/javascript/longestSubStringKdistinct.js
@@ -1,3 +1,5 @@
+const assert = require('assert')
+
 const longest_substring_with_k_distinct = function(str, k) {
     let distinctChars = {}
     let start = 0
@@ -23,3 +25,15 @@ const longest_substring_with_k_distinct = function(str, k) {
     return maxSubstring;
   };
   
+  const string1 = "araaci"
+  const string2 = "cbbebi"
+  const distinct1 = 2
+  const distinct2 = 1
+  const distinct3 = 3
+  const result1 = 4
+  const result2 = 2
+  const result3 = 5
+
+  assert.strictEqual(longest_substring_with_k_distinct(string1, distinct1), result1, `Expected Max Length of substring to be: ${result1}`)
+  assert.strictEqual(longest_substring_with_k_distinct(string1, distinct2), result2, `Expected Max Length of substring to be: ${result2}`)
+  assert.strictEqual(longest_substring_with_k_distinct(string2, distinct3), result3, `Expected Max Length of substring to be: ${result3}`)
