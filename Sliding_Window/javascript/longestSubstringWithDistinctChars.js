cb787162bfb214485839c61fbc4851266dc7fe90
Kevin Molina <kevmo1987@gmail.com>
2023-09-08 16:07:13 -0700

    added tests

--- Sliding_Window/javascript/longestSubstringWithDistinctChars.js
+++ Sliding_Window/javascript/longestSubstringWithDistinctChars.js
@@ -1,3 +1,5 @@
+const assert = require('assert')
+
 const non_repeat_substring = function(str) {
     let max_sub = -1
     let chars = {}
@@ -15,3 +17,10 @@ const non_repeat_substring = function(str) {
     }
     return max_sub;
   };
+
+  const string1 = "abbbb"
+  const string2 = "abccde"
+  const result1 = 2
+  const result2 = 3
+  assert.strictEqual(non_repeat_substring(string1), result1, `Expected result: ${result1} as longest substring with distinct chars`)
+  assert.strictEqual(non_repeat_substring(string2), result2, `Expected result: ${result2} as longest substring with distinct chars`)
